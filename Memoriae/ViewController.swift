3df9ba716f69cf206fbc43607b280fa53e2feaaa
panandafog <panandafog@gmail.com>
2020-04-01 02:29:36 +0300

    project init, added pods & swiftlint

---
+++ Memoriae/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Memoriae
+//
+//  Created by panandafog on 31.03.2020.
+//  Copyright © 2020 SheetCode Team. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
