2d394fbcbeef63deffbd7234c3722f556299fb28
shahbazly <shahbazly.dev@gmail.com>
2020-05-10 21:08:12 +0300

    WMSNumbers GUI setup

--- Memoriae/Screen/Levels/WMSNumbers/WMSNumbersViewController.swift
+++ Memoriae/Screen/Levels/WMSNumbers/WMSNumbersViewController.swift
@@ -9,63 +9,82 @@
 import UIKit
 
 class WMSNumbersViewController: UIViewController, LevelViewController {
+    
     var level: Level?
     var difficulty: Float?
-    var counter = 0
-    var counterRight = 0
-    var array = [Int]()
-    var right = 0
-    var buttons = [UIButton]()
+    var numbersCount = 0
+    var randNumbers = [Int]()
     var userAnswers = [Int]()
+    var buttons = [UIButton]()
     
-    @IBOutlet private var bigCircle: UIButton!
-    
+    @IBOutlet private var mainButton: UIButton!
     @IBOutlet private var button0: UIButton!
-    
     @IBOutlet private var button1: UIButton!
-    
     @IBOutlet private var button2: UIButton!
-    
     @IBOutlet private var button3: UIButton!
-    
     @IBOutlet private var button4: UIButton!
-    
     @IBOutlet private var button5: UIButton!
-    
     @IBOutlet private var button6: UIButton!
-    
     @IBOutlet private var button7: UIButton!
-    
     @IBOutlet private var button8: UIButton!
-    
     @IBOutlet private var button9: UIButton!
-    
-    
-    @IBAction private func huisosat(sender: UIButton) {
-        sender.setTitle("StarHHhhhhh", for: .normal)
-        Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(randomNumbers), userInfo: nil, repeats: true)
-        
-        //write there
-        
-    }
+    @IBOutlet private var submit: UIButton!
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+
+        mainButton.layer.cornerRadius = 0.5 * mainButton.bounds.size.width
+        mainButton.layer.masksToBounds = true
+        mainButton.setTitle("Start", for: .normal)
+        mainButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 72)
+        mainButton.setTitleColor(.white, for: .init())
+        mainButton.backgroundColor = ColorScheme.tintColor
+        submit.backgroundColor = ColorScheme.tintColor
+
+        setupButtons()
         
-        let testButton = UIButton(type: .system)
-        testButton.titleLabel?.text = "RANDOM KAKOY NIBUD NAPISHI"
-        testButton.center = CGPoint(x: 100, y: 100)
-        testButton.tintColor = .black
-        self.view.addSubview(testButton)
-        self.view.bringSubviewToFront(testButton)
-        
-        let button = UIButton(frame: CGRect(x: 100, y: 100, width: 100, height: 50))
-        button.backgroundColor = .green
-        button.setTitle("Test Button", for: .init())
-        button.addTarget(self, action: #selector(WMSNumbersViewController.checker(sender:)), for: .touchUpInside)
-        self.view.addSubview(button)
-        
-        
+    }
+    
+    @IBAction private func startGame(sender: UIButton) {
+        sender.setTitle("Go!", for: .normal)
+        Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(randomNumbers), userInfo: nil, repeats: true)
+    }
+    
+    @IBAction private func exit(_ sender: Any) {
+
+        let storyBoard = UIStoryboard(name: "Main", bundle: nil)
+        guard let newViewController = storyBoard.instantiateViewController(identifier: "entry") as? UITabBarController else {
+            return
+        }
+
+        navigationController?.pushViewController(newViewController, animated: true)
+    }
+    
+    func setTest(level: Level, difficulty: Float) {
+        self.level = level
+        self.difficulty = difficulty
+    }
+    
+    @objc func randomNumbers(timer: Timer) {
+        if numbersCount > 5 {
+            timer.invalidate()
+            mainButton.setTitle("That's all", for: .normal)
+            numbersCount = 0
+            
+            mainButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 36)
+            for butt in buttons {
+                butt.addTarget(self, action: #selector(WMSNumbersViewController.checker(sender:)), for: .touchUpInside)
+            }
+            
+            return
+        }
+        let random = Int.random(in: 0..<10)
+        randNumbers.append(random)
+        mainButton.setTitle(String(random), for: .normal)
+        numbersCount += 1
+    }
+    
+    func setupButtons() {
         buttons.append(button0)
         buttons.append(button1)
         buttons.append(button2)
@@ -76,67 +95,33 @@ class WMSNumbersViewController: UIViewController, LevelViewController {
         buttons.append(button7)
         buttons.append(button8)
         buttons.append(button9)
+        
         for index in 0...9 {
             buttons[index].setTitle(String(index), for: .normal)
-            //buttons[index].addTarget(self, action: #selector(WMSNumbersViewController.checker(sender:)), for: .touchUpInside)
-            //buttons[index].setIndex(index: index)
+            buttons[index].layer.cornerRadius = 0.5 * buttons[index].bounds.size.width
+            buttons[index].layer.masksToBounds = true
+            buttons[index].titleLabel?.font = UIFont.boldSystemFont(ofSize: 36)
+            buttons[index].setTitleColor(.white, for: .init())
+            buttons[index].backgroundColor = ColorScheme.tintColor
         }
-        bigCircle.layer.cornerRadius = 75
-        bigCircle.layer.masksToBounds = true
-        bigCircle.setTitle("Start", for: .normal)
-    }
-    
-    func setTest(level: Level, difficulty: Float) {
-        self.level = level
-        self.difficulty = difficulty
-    }
-    
-    @objc func randomNumbers(timer: Timer) {
-        if counter > 5 {
-            timer.invalidate()
-            bigCircle.setTitle("That's all", for: .normal)
-            counter = 0
-            for index in 0...9 {
-                buttons[index].isHidden = false
-            }
-            return
-            
-        }
-        let random = Int.random(in: 0..<10)
-        array.append(random)
-        bigCircle.setTitle(String(random), for: .normal)
-        counter += 1
-    }
-    
-    func checkAnswers() {
-        for index in 0...9 {
-            userAnswers.append(array[index])
-            if String(array[index]) == buttons[index].title(for: .init()) {
-                counterRight += 1
-            }
-        }
-        if counter != counterRight {
-            bigCircle.setTitle("GAME OVER", for: .normal)
-        }
-    }
-    
-    @objc func checker(sender: Any) {
-        /*
-         if array[counter] == sender.getIndex() {
-         right += 1
-         sender.tintColor = .systemGreen
-         sleep(1)
-         sender.tintColor = .systemOrange
-         } else {
-         sender.tintColor = .systemRed
-         sleep(1)
-         sender.tintColor = .systemOrange
-         }
-         counter += 1
-         */
         
+        submit.layer.masksToBounds = true
+        submit.layer.cornerRadius = 0.5 * submit.bounds.size.height
+        submit.titleLabel?.text = "Submit"
+        submit.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
+        submit.setTitleColor(.white, for: .init())
+        
+        
+    }
+
+    @objc func checker(sender: UIButton) {
+        //проверка после ввода последовательности чисел
         //sender.tintColor = .systemRed
-        print("Tural pochinil")
+        print(sender.title(for: .normal))
+    }
+    
+    func setTest(level: Level, difficultyIndex: Int) {
+        //
     }
     
 }
